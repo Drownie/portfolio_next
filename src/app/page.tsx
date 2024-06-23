7c95e9c4880b692641b3148e3bc2dd8cb1fb3786
abrahammahanaim02 <abrahammahanaim02@gmail.com>
2024-06-23 12:04:47 +0000

    add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/about";
 import Footer from "@/components/footer";
 import Projects from "@/components/projects";
 import ScrollToTop from "@/components/scroll_to_top";
+import Contact from "@/components/contact";
 
 export default function Home () {
     return (
@@ -13,6 +14,7 @@ export default function Home () {
             <div className="border-[.5px] border-amber-700 border-solid w-4/5 ml-[10%] my-10"></div>
             <About />
             <Projects />
+            <Contact />
             <Footer />
             <ScrollToTop />
         </main>
